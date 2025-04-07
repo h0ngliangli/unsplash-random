075f27f48c59d215544f6d79bd90f0d570e46367
Hongliang Li <woodyli1982@gmail.com>
2025-04-06 23:00:02 -0700

    增加default

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,83 @@
 <script setup>
 import { onMounted, ref } from "vue"
-const refData = ref("loading...")
+import config from "./config.js"
 
-const load = async () => {
-  console.log("Loading...")
+const refModel = ref({
+  countDown: config.intervalSeconds,
+  description: "",
+  url: "/default.jpg",
+  username: "",
+  userprofileUrl: "",
+  likes: 0,
+  views: 0,
+  downloads: 0,
+  errorCode: 0,
+  errorMessage: "",
+})
+
+//
+
+// 读取随机图片
+const reload = async () => {
+  const counter = setInterval(() => {
+    refModel.value.countDown--
+    if (refModel.value.countDown <= 0) {
+      clearInterval(counter)
+      refModel.value.countDown = config.intervalSeconds
+      reload()
+    }
+  }, 1000)
   const response = await fetch("https://api.unsplash.com/photos/random", {
     headers: {
-      Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
+      Authorization: `Client-ID ${config.unsplashAccessKey}`,
     },
   })
   if (!response.ok) {
-    console.error("Error fetching data:", response.statusText)
+    refModel.value.errorCode = response.status
+    refModel.value.errorMessage = response.statusText
+    // console.error("Error fetching data:", response.statusText)
     return
   }
+  refModel.value.errorCode = 0
+  refModel.value.errorMessage = ""
   const data = await response.json()
-  console.log(data)
-  refData.value = data
+  // console.log(data)
+  refModel.value.description = data.description || data.alt_description || ""
+  refModel.value.url = data.urls.full
+  refModel.value.username = data.user.name
+  refModel.value.userprofileUrl = data.user.profile_image.medium
+  refModel.value.likes = data.likes
+  refModel.value.views = data.views
+  refModel.value.downloads = data.downloads
+  refModel.value.countDown = config.intervalSeconds
 }
 onMounted(async () => {
-  await load()
-  setInterval(async () => {
-    await load()
-  }, 2 * 60 * 1000)
+  reload()
 })
 </script>
 
 <template>
   <div>
-    <img
-      :src="refData.urls?.full"
-      alt="Random Unsplash Image"
+    <div
       class="wallpaper"
-    />
+      :style="{ backgroundImage: 'url(' + refModel.url + ')' }"
+    ></div>
+    <!-- <img :src="refModel.url" class="wallpaper" /> -->
     <div class="desc">
-      <h1>{{ refData?.description }}</h1>
-      <p>By {{ refData?.user?.name }}</p>
-      <p>Likes: {{ refData?.likes }}</p>
-      <p>Views: {{ refData?.views }}</p>
-      <p>Downloads: {{ refData?.downloads }}</p>
+      <div v-if="refModel.errorCode === 0">
+        <h1>{{ refModel.description }}</h1>
+        <p>
+          By <img :src="refModel.userprofileUrl" width="16px" />
+          {{ refModel.username }}
+        </p>
+        <p>Likes: {{ refModel.likes }}</p>
+        <p>Views: {{ refModel.views }}</p>
+        <p>Downloads: {{ refModel.downloads }}</p>
+      </div>
+      <div v-else>
+        <p>Error: {{ refModel.errorCode }} {{ refModel.errorMessage }}</p>
+      </div>
+      <p>Next in {{ refModel.countDown }}</p>
     </div>
   </div>
 </template>
@@ -46,7 +86,9 @@ onMounted(async () => {
 .wallpaper {
   width: 100vw;
   height: 100vh;
-  object-fit: cover;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
 }
 .desc {
   /* position this to the top left */
@@ -60,5 +102,13 @@ onMounted(async () => {
   font-size: 20px;
   font-family: "Courier New", Courier, monospace;
   text-align: left;
+}
+.desc h1 {
+  font-size: 20px;
+}
+
+.desc p {
+  font-size: 14px;
+  margin: 0;
 }
 </style>
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  intervalSeconds: 120,
+  unsplashAccessKey: "q76eihhI1m7Xf488O-TziD9QY_Rud9Fb8KueGsFGM4A",
+}
