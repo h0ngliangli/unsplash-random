036eee0ab2bd1da9b0c31b9919dc0787caace1c4
Hongliang Li <woodyli1982@gmail.com>
2025-04-07 11:15:32 -0700

    update server port configuration to use environment variable

--- serve.js
+++ serve.js
@@ -1,10 +1,10 @@
-import  express from "express"
+import express from "express"
+import process from "process"
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static("dist"))
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
